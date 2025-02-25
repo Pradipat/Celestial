8c6e972427d6ec8b4f9811c8856e677e7ed0cd97
Pradipat <P0861719933@gmail.com>
2025-02-25 13:08:11 +0700

    re-deploy

--- app/api/update-category/route.js
+++ app/api/update-category/route.js
@@ -1,6 +1,4 @@
 import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
 import connectDB from "@/utils/db";
 import Portfolio from "@/models/Portfolio";
 import { getServerSession } from "next-auth/next";
